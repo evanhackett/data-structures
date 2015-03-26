db3201457506d8adef42b83880a0405237fcbc13
evanhackett <evanwhackett@gmail.com>
2015-03-25 17:27:36 -0700

    completed functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,27 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {front : 0, back : 0, storage : {}};
+  _.extend(queue, queueMethods);
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  },
+  dequeue : function(value) {
+    if (this.size() === 0) {return undefined;}
+    var temp = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return temp;
+  },
+  size : function() {
+    return this.back - this.front;
+  }
+};
 
 
 
